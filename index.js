8f2aaa7315afdcbe1d49f64470517a5d0b63dd06
Walter Alvarado <yovaemperor@gmail.com>
2021-10-30 13:19:58 +0200

    add Get and PUT

--- index.js
+++ index.js
@@ -16,6 +16,37 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+// read a file -- /files/tim --> tim.txt
+// GET
+app.get('/files/:name', async (req, res) => {
+  try {
+    const content = await read(`./files/${req.params.name}.txt`, 'utf8');
+    res.send(content);
+  } catch (error) {
+    res.status(404).send({ ok: false, message: 'file not found' });
+  }
+});
+
+// update a file
+// PUT
+app.put('/files/:name', async (req, res) => {
+  const path = `./files/${req.params.name}.txt`;
+  if (!fs.existsSync(path)) {
+    res
+      .status(409)
+      .send({ ok: false, message: 'file with that name does not exist!' });
+    return;
+  }
+  try {
+    await write(path, req.body);
+    res.send({ ok: true, message: `file ${path} updated successfully` });
+  } catch (error) {
+    res
+      .status(500)
+      .send({ ok: false, message: 'error writing file', err: error });
+  }
+});
+
 app.use(
   morgan('combined', {
     stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {
@@ -31,6 +62,9 @@ if (config.MODE === 'development') {
 app.use('/api', api);
 
 app.use('/', express.static(path.join(__dirname, config.STATIC_DIR)));
+
+
+
 
 /* eslint-disable */
 app.use((err, req, res, next) => {
