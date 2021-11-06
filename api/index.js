0258c3a65c4dda53332562b80c4f22c8d53a1c9c
Walter Alvarado <yovaemperor@gmail.com>
2021-11-06 15:07:21 +0100

    Update index.js
    refactor

--- api/index.js
+++ api/index.js
@@ -1,20 +1,61 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
-const channelRoutes = require('./routes/channels');
-const messageRoutes = require('./routes/messages');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
 
-const router = express.Router();
+const config = require('./config');
+const routes = require('./routes/index');
+const registerRoute = require('./routes/register');
+const loginRoute = require('./routes/login');
 
-router.use((req, res, next) => {
-  console.log('api!');
-  next();
+const authCheck = require('./middleware/auth-check');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(
+  morgan('combined', {
+    stream: fs.createWriteStream(path.join(__dirname, 'access.log'), {
+      flags: 'a',
+    }),
+  }),
+);
+
+if (config.MODE === 'development') {
+  app.use(morgan('dev'));
+}
+
+//register
+app.use('/api/register', registerRoute);
+
+//login
+app.use('/api/login', loginRoute);
+
+app.use(authCheck);
+
+app.use('/api', routes);
+
+app.use('/', express.static(path.join(__dirname, '..', config.STATIC_DIR)));
+
+/* eslint-disable */
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).end();
 });
 
-router.get('/', (req, res) => {
-  res.send('api!');
+app.listen(config.PORT, (err) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(
+      `listening at http://localhost:${config.PORT} (${config.MODE} mode)`,
+    );
+  }
 });
-
-// use routes
-router.use('/channels', channelRoutes);
-router.use('/', messageRoutes);
-
-module.exports = router;
